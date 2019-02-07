bfd8566599301d12298a8aeb542e99306235a607
astegner <allison.stegner@gmail.com>
2019-02-07 12:23:35 -0800

    add package requirements to map_dl

--- R/map_dl.R
+++ R/map_dl.R
@@ -1,7 +1,7 @@
 #' Map a Neotoma Download Obect
 #'
 #' Function to map location of Neotoma sites
-#' @param tax_dec_dl a Neotoma download object
+#' @param dl_obj a Neotoma download object
 #' @param X numeric; longitude
 #' @param Y numeric; latitude
 #' @param add logical; should points be added to an existing map?
@@ -12,7 +12,8 @@
 #' @author M. Allison Stegner
 #' @export
 
-map_dl<-function(tax_dec_dl,X,Y,add,color,label.sites,return.table){
+map_dl<-function(dl_obj,X,Y,add,color,label.sites,return.table){
+	require(maps)
 	if (add==FALSE){
 		map("world",xlim=X,ylim=Y)
 	} 
@@ -21,13 +22,13 @@ map_dl<-function(tax_dec_dl,X,Y,add,color,label.sites,return.table){
 	dataset.id<-c()
 	site.name<-c()
 	site.id<-c()
-	for (i in 1:length(tax_dec_dl)){
-		long[i]<-tax_dec_dl[[i]]$dataset$site.data$long
-		lat[i]<-tax_dec_dl[[i]]$dataset$site.data$lat
+	for (i in 1:length(dl_obj)){
+		long[i]<-dl_obj[[i]]$dataset$site.data$long
+		lat[i]<-dl_obj[[i]]$dataset$site.data$lat
 		points(long[i],lat[i],pch=16,col=color)
-		dataset.id[i]<-tax_dec_dl[[i]]$dataset$dataset.meta$dataset.id
-		site.name[i]<-tax_dec_dl[[i]]$dataset$site.data$site.name
-		site.id[i]<-tax_dec_dl[[i]]$dataset$site.data$site.id
+		dataset.id[i]<-dl_obj[[i]]$dataset$dataset.meta$dataset.id
+		site.name[i]<-dl_obj[[i]]$dataset$site.data$site.name
+		site.id[i]<-dl_obj[[i]]$dataset$site.data$site.id
 	}
 	if (label.sites==TRUE){
 		text(long,lat,site.id,cex=0.5,pos=4,offset=0.2)
