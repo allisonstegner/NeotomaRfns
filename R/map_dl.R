3a96bc407d609f0ed8dd4c1846f66fac2e8e38c5
astegner <allison.stegner@gmail.com>
2019-02-07 12:43:21 -0800

    edited default params in map_dl.R

--- R/map_dl.R
+++ R/map_dl.R
@@ -5,15 +5,16 @@
 #' @param X numeric; longitude
 #' @param Y numeric; latitude
 #' @param add logical; should points be added to an existing map?
-#' @param color what color should the points be?
+#' @param col what color should the points be?
 #' @param label.sites logical; should dataset ids be added as text labels?
 #' @param return.table logical; should a matrix containing site name, dataset id, long, and lat be returned?
 #' @return A four column matrix containing site name, dataset ids, longitudes, and latitudes
 #' @author M. Allison Stegner
 #' @export
 
-map_dl<-function(dl_obj,X,Y,add,color,label.sites,return.table){
+map_dl<-function(dl_obj,X,Y,add,col=NULL,label.sites=FALSE,return.table=TRUE){
 	require(maps)
+		
 	if (add==FALSE){
 		map("world",xlim=X,ylim=Y)
 	} 
@@ -25,7 +26,8 @@ map_dl<-function(dl_obj,X,Y,add,color,label.sites,return.table){
 	for (i in 1:length(dl_obj)){
 		long[i]<-dl_obj[[i]]$dataset$site.data$long
 		lat[i]<-dl_obj[[i]]$dataset$site.data$lat
-		points(long[i],lat[i],pch=16,col=color)
+		if (!is.null(col)) points(long[i],lat[i],pch=16,col=col)
+		else points(long[i],lat[i],pch=16)
 		dataset.id[i]<-dl_obj[[i]]$dataset$dataset.meta$dataset.id
 		site.name[i]<-dl_obj[[i]]$dataset$site.data$site.name
 		site.id[i]<-dl_obj[[i]]$dataset$site.data$site.id
