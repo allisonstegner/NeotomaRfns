4ecde2a93aee8e38178eaaefc0a9a192757f9b06
astegner <allison.stegner@gmail.com>
2019-02-08 08:32:28 -0800

    Adding ts_min_duration function

---
+++ R/ts_min_duration.R
@@ -0,0 +1,35 @@
+#' Function to identify datasets that are of sufficient duration
+#'
+#' This function identifies Neotoma dastasets which have at least a minimum amount of temporal coverage
+#' @param dl_obj Neotoma download object
+#' @param min.time numeric; minimum duration, in years. Sites with number of samples less than min.samples are excluded
+#' @param agemodels list object of agemodels. Can be provided by user, or default age model from Neotoma is used.
+#' @return A list of Neotoma dataset ids for sites that have adequate duration.
+#' @author M. Allison Stegner
+#' @export
+
+
+ts_min_duration<-function(dl_obj,min.time,agemodels){
+	dataset.ids<-c() # this will be filled with the Neotoma dataset ids for datasets that meet the inclusion criteria
+	for (i in 1:length(dl_obj)) {
+		sitei<-dl_obj[[i]]
+		
+		# choose the default chronology
+		if (is.null(agemodels)){
+			chroncont<-get_chroncontrol(sitei)
+			default.chronology<-chroncont$meta$name		
+			chronj<-sitei$chronologies[[i.chrons[default.chronology]]]	
+		} else {	
+			chronology.name<-agemodels[agemodels[,1] %in% names(dl_obj[i]),3]
+			chronj<-sitei$chronologies[[chronology.name]]		
+		}		
+		dur<-max(chronj$age,na.rm=T)-min(chronj$age,na.rm=T)
+		
+		if (dur<min.time){ 
+			dataset.ids[i]<-NA
+		} else {
+			dataset.ids[i]<-sitei$dataset$dataset.meta$dataset.id
+		}
+	}
+		return(adequate.time=dataset.ids)
+}
