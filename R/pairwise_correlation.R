bf927d5d6a90f266582f2dd8d268166e9f46928a
Allison Stegner <allison.stegner@gmail.com>
2019-08-05 15:50:35 -0400

    perfecting pairwise_correlation function

--- R/pairwise_correlation.R
+++ R/pairwise_correlation.R
@@ -8,18 +8,25 @@
 #' @export
 
 
-pairwise_correlation<-function(datamatx,method){
+pairwise_correlation<-function(datamatx,method,pval.adjust=F){
 	rhos<-matrix(NA,nr=ncol(datamatx),nc=ncol(datamatx))
 	ps<-matrix(NA,nr=ncol(datamatx),nc=ncol(datamatx))
-	for (k in 1:ncol(datamatx)){
+	for (k in 1:(ncol(datamatx)-1)){
 		vect1<-datamatx[,k]
-		for (j in k:ncol(datamatx)){
+		for (j in (k+1):ncol(datamatx)){
 			vect2<-datamatx[,j]
 			cor.out<-cor.test(vect1,vect2,method=method)
 			rhos[j,k]<-cor.out$estimate[[1]]
-			ps[j,k]<-cor.out$p.value[[1]]
+			
+			if(pval.adjust==T){
+				n.tests<-(ncol(datamatx)*(ncol(datamatx)+1))/2
+				ps[j,k]<-p.adjust(cor.out$p.value[[1]],method="holm",n=n.tests)
+			} else {
+				ps[j,k]<-cor.out$p.value[[1]]
+			}
 		}
 	}
+	
 	rownames(rhos)<-colnames(rhos)<-rownames(ps)<-colnames(ps)<-colnames(datamatx)
 	out<-list(rhos=rhos,ps=ps)
 	return(out)
